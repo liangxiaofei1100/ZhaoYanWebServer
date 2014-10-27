358a6554f19ff1fa4dc4bb0d050a20aa7da5b7d7
liangxiaofei <liangxiaofei1100@163.com>
2014-10-27 14:20:16 +0800

    Add userid in user info.

--- src/com/zhaoyan/webserver/userinfomanage/UserInfoUtil.java
+++ src/com/zhaoyan/webserver/userinfomanage/UserInfoUtil.java
@@ -9,12 +9,14 @@ import com.zhaoyan.webserver.db.DBData.UserInfoTable;
 public class UserInfoUtil {
 
 	public static String userInfo2Json(Map<String, Object> map) {
+		int id = (Integer)map.get(UserInfoTable.ID);
 		String userName = (String) map.get(UserInfoTable.USER_NAME);
 		String nickName = (String) map.get(UserInfoTable.NICKNAME);
 		String email = (String) map.get(UserInfoTable.EMAIL);
 		int jifen = (Integer) map.get(UserInfoTable.JIFEN);
 		String phone = (String) map.get(UserInfoTable.PHONE);
 		JSONObject user = new JSONObject();
+		user.put("id", id);
 		user.put("userName", userName);
 		user.put("nickname", nickName);
 		user.put("email", email);
